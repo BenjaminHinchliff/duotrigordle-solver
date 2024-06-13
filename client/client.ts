09d8bc2925abeb0a0f441883f680ca9c3281fc24
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2024-06-13 00:16:41 -0700

    fix usage bug in client

--- client/client.ts
+++ client/client.ts
@@ -1,7 +1,7 @@
 import { DuotrigordleSolver } from './solver'
 
 (async () => {
-  const solver = new DuotrigordleSolver(false)
+  const solver = new DuotrigordleSolver(["RAISE"], false)
   await solver.setup()
   await solver.solveDuotrigordle()
   await solver.cleanup()
